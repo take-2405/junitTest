4071ab64b0d57b9ad11c1b2f8df516a111f46189
take-2405 <take12.wa@icloud.com>
2022-01-20 15:46:32 +0900

    ミッション4

--- src/main/java/com/example/demo/JudgeUtils.java
+++ src/main/java/com/example/demo/JudgeUtils.java
@@ -6,7 +6,8 @@ import java.util.Date;
 public class JudgeUtils extends MiraiPrjApplication {
 
 	// 定数
-	public static final int AGE = 17;
+	public static final int AGE = 18;
+	public static final int PLAYAGE = 17;
 
 	/**
 	 * 課金対象か判定するメソッド<br>
@@ -58,7 +59,7 @@ public class JudgeUtils extends MiraiPrjApplication {
 		// 現在の年月日のみをセット
 		compareCal.set(curYear, curMonth, curDate);
 		// 年からプレイ可能年齢を引く
-		compareCal.add(Calendar.YEAR, -AGE);
+		compareCal.add(Calendar.YEAR, -PLAYAGE);
 
 		// 生年月日を設定
 		Calendar birthCal = Calendar.getInstance();
